577c74a5a92ecd18c424547e2bb583937ad33cc4
Facundo Duran <108543916+facundonubi@users.noreply.github.com>
2023-10-09 18:06:52 -0300

    First commit

---
+++ Lunes/09102023/classes/Academia/Alumno.py
@@ -0,0 +1,43 @@
+
+class Alumno:
+  def __init__(self, nombre, apellido, edad, dni, materias):
+    self.nombre = nombre
+    self.apellido = apellido
+    self.edad = edad
+    self.dni = dni
+    self.materias = materias
+
+  def obtener_promedio_anual(self, anio):
+    materias_anuales = list()
+    for materia in self.materias:
+      if materia.anio == anio:
+        materias_anuales.append(materia.nota)
+    return sum(materias_anuales) // len(materias_anuales)
+
+  def obtener_promedio_general(self):
+    promedio_general = list()
+    for materia in self.materias:
+      promedio_general.append(materia.nota)
+    return sum(promedio_general) // len(promedio_general)
+
+  def mejor_materia(self):
+    notas = [
+        materia.nota for materia in self.materias
+    ]
+    nota_maxima = max(notas)
+    for materia in self.materias:
+      if materia.nota == nota_maxima:
+        print(materia.nombre, materia.nota, materia.anio)
+
+  def materias_desaprobadas(self, aprueba_con):
+    desaprobadas = list()
+    for materia in self.materias:
+      if materia.nota < aprueba_con:
+        desaprobadas.append(
+            {
+                "nombre": materia.nombre,
+                "nota":materia.nota,
+                "año":materia.anio,
+            }
+        )
+    return desaprobadas
---
+++ Lunes/09102023/main.py
@@ -0,0 +1,28 @@
+from classes.Academia.Academico import Academico
+from classes.Academia.Materia import Materia
+from classes.Academia.Alumno import Alumno
+
+lista_materias_1 = [
+    Materia("Historia",2021, 7),
+    Materia("Matemáticas", 2021, 5),
+    Materia("Historia",2022, 7),
+    Materia("Matemáticas", 2022, 7),
+    Materia("Historia",2023, 7),
+    Materia("Matemáticas", 2023, 4),
+    Materia("Geografía",2021, 2),
+    Materia("Conocimiento del Medio", 2023, 5),
+]
+lista_materias_2 = [
+    Materia("Historia",2021, 4),
+    Materia("Matemáticas", 2021, 6),
+    Materia("Historia",2022, 9),
+    Materia("Matemáticas", 2022, 6),
+    Materia("Historia",2023, 8),
+    Materia("Matemáticas", 2023, 5),
+    Materia("Geografía",2021, 5),
+    Materia("Conocimiento del Medio", 2023, 10),
+]
+alumno1 = Alumno("Facundo", "Duran", 24, 1, lista_materias_1)
+alumno2 = Alumno("Camila","Perez", 26, 2, lista_materias_2)
+academia1 = Academico("UBA", [alumno1, alumno2])
+print(academia1.alumnos_graduados(4))
